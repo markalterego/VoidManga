2a888121ac8455c3e4d3be75881321b470a14796
markalterego <122254234+markalterego@users.noreply.github.com>
2025-03-20 22:06:39 +0200

    extended logging output

--- output/logtoconsole.js
+++ output/logtoconsole.js
@@ -20,58 +20,56 @@ function logMAL (options, lists) {
 
         switch (options) {
             case 'anime_watching':
-                //console.log('\n||\n|| = Animelist =\n||'); // logging MAL personal anime titles
                 console.log('\n||\n|| (Watching)\n||'); // logging titles marked as 'watching'
                 for (let i = 0; i < animelist[0].length; i++) { 
                     console.log(`|| - ${animelist[0][i].node.title} ( ${animelist[0][i].list_status.num_episodes_watched} / ${animelist[0][i].node.num_episodes ? animelist[0][i].node.num_episodes : 'unknown'} )`);
-                    if (i===(animelist[0].length-1)) console.log('||');
+                    if (i===(animelist[0].length-1)) if (animelist[0].length < 5) console.log('||'); else console.log(`||\n|| Currently watching ${i+1} different anime\n||`);
                 }
                 break;
             case 'anime_completed':
                 console.log('\n||\n|| (Completed)\n||'); // logging titles marked as 'completed'
                 for (let i = 0; i < animelist[1].length; i++) { 
                     console.log(`|| - ${animelist[1][i].node.title}`);
-                    if (i===(animelist[1].length-1)) console.log('||');
+                    if (i===(animelist[1].length-1)) if (animelist[1].length < 5) console.log('||'); else console.log(`||\n|| You have watched ${i+1} different anime\n||`);
                 }
                 break;
             case 'manga_reading':
-                //console.log('\n||\n|| = Mangalist =\n||'); // logging MAL personal manga titles
                 console.log('\n||\n|| (Reading)\n||'); // logging titles marked as 'reading'
                 for (let i = 0; i < mangalist[0].length; i++) { 
                     console.log(`|| - ${mangalist[0][i].node.title} ( ${mangalist[0][i].list_status.num_chapters_read} / ${mangalist[0][i].node.num_chapters ? mangalist[0][i].node.num_chapters : 'unknown'} )`);
-                    if (i===(mangalist[0].length-1)) console.log('||');
+                    if (i===(mangalist[0].length-1)) if (mangalist[0].length < 5) console.log('||'); else console.log(`||\n|| You are reading ${i+1} different manga\n||`);
                 }
                 break;
             case 'manga_completed':
                 console.log('\n||\n|| (Completed)\n||'); // logging titles marked as 'completed'
                 for (let i = 0; i < mangalist[1].length; i++) { 
                     console.log(`|| - ${mangalist[1][i].node.title}`);
-                    if (i===(mangalist[1].length-1)) console.log('||');
+                    if (i===(mangalist[1].length-1)) if (mangalist[1].length < 5) console.log('||'); else console.log(`||\n|| You have read ${i+1} mangas\n||`);
                 }
                 break;
             case 'all':
+                let totalEntries = 0;
                 console.log('\n||\n|| = Animelist =\n||'); // logging MAL personal anime titles
                 console.log('|| (Watching)\n||'); // logging titles marked as 'watching'
                 for (let i = 0; i < animelist[0].length; i++) { 
                     console.log(`|| - ${animelist[0][i].node.title} ( ${animelist[0][i].list_status.num_episodes_watched} / ${animelist[0][i].node.num_episodes ? animelist[0][i].node.num_episodes : 'unknown'} )`);
-                    if (i===(animelist[0].length-1)) console.log('||');
+                    if (i===(animelist[0].length-1)) { totalEntries+=i+1; console.log('||') }; 
                 }
                 console.log('|| (Completed)\n||'); // logging titles marked as 'completed'
                 for (let i = 0; i < animelist[1].length; i++) { 
                     console.log(`|| - ${animelist[1][i].node.title}`);
-                    if (i===(animelist[1].length-1)) console.log('||');
+                    if (i===(animelist[1].length-1)) { totalEntries+=i+1; console.log('||') };
                 }
-
                 console.log('\n||\n|| = Mangalist =\n||'); // logging MAL personal manga titles
                 console.log('|| (Reading)\n||'); // logging titles marked as 'reading'
                 for (let i = 0; i < mangalist[0].length; i++) { 
                     console.log(`|| - ${mangalist[0][i].node.title} ( ${mangalist[0][i].list_status.num_chapters_read} / ${mangalist[0][i].node.num_chapters ? mangalist[0][i].node.num_chapters : 'unknown'} )`);
-                    if (i===(mangalist[0].length-1)) console.log('||');
+                    if (i===(mangalist[0].length-1)) { totalEntries+=i+1; console.log('||') };
                 }
                 console.log('|| (Completed)\n||'); // logging titles marked as 'completed'
                 for (let i = 0; i < mangalist[1].length; i++) { 
                     console.log(`|| - ${mangalist[1][i].node.title}`);
-                    if (i===(mangalist[1].length-1)) console.log('||');
+                    if (i===(mangalist[1].length-1)) { totalEntries+=i+1; console.log(`||\n|| In total your list consists of ${totalEntries} entries\n||`) };
                 }
                 break;
             default:
