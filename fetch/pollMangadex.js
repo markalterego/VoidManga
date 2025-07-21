27a40e6a3458bdb26f43a348e955572b4a0cde1b
markalterego <122254234+markalterego@users.noreply.github.com>
2025-07-21 22:27:56 +0300

    first basic version of custom search complete

--- fetch/pollMangadex.js
+++ fetch/pollMangadex.js
@@ -5,29 +5,37 @@ import axios from 'axios';
     Options for fetchChapters() custom search:
         -MAL_list - anime/manga = 0/1
         -MAL_status - watching/reading, completed, on-hold, dropped, plan-to-watch/plan-to-read = 0/1/2/3/4
-        
-        -limit_manga = 0-100, undefined for default behavior (10)
-        -limit_chapter = 0-100, undefined for default behavior (10)
-        -translatedLanguage = ['en','es',etc...], undefined for all languages
+        -limit_manga: 0-100, undefined for default behavior (10)
+        -limit_chapter: 0-100, undefined for default behavior (10)
+        -mangaOrderType: 'title', 'year', 'createdAt', 'updatedAt', 'latestUploadedChapter', 'followedCount', 'relevance'
+        -chapterOrderType: 'createdAt', 'updatedAt', 'publishAt', 'readableAt', 'volume', 'chapter'
+        -mangaOrderDirection: 'asc', 'desc' - e.g. using 'desc' when order['relevance']: 'desc' sorts by most relevant to least relevant
+        -chapterOrderDirection: 'asc, 'desc'
         -contentRating: ['safe','suggestive','erotica','pornographic'], undefined for default behavior (all expect pornographic)
+        -chapterTranslatedLanguage: ['en','es','etc...'], undefined for all languages
 
     Options are saved as json:
     const options = {
         variable: ['value'],
         ...
     };
-    params: filters
+    params: options
 
     const can be spreaded to change values inside:
     options = { ...options, variable: 'short', etc...}
 */  
 
 const options = {
-    MAL_list: 1, // 0, 1
-    MAL_status: 0, // 0 - 4 
+    MAL_list: 1, // 0 or 1
+    MAL_status: 1, // 0 - 4 
     limit_manga: undefined, // default: 10, min: 0, max is 100
     limit_chapter: undefined, // default: 10, min: 0, max is 100 
-    orderType: 'relevance', // check what was the default sorting method for the api!!!
+    mangaOrderType: 'relevance', // 'title', 'year', 'createdAt', 'updatedAt', 'latestUploadedChapter', 'followedCount', 'relevance'
+    chapterOrderType: 'chapter', // 'createdAt', 'updatedAt', 'publishAt', 'readableAt', 'volume', 'chapter'
+    mangaOrderDirection: 'desc', // 'asc', 'desc'
+    chapterOrderDirection: 'asc', // 'asc', 'desc'
+    contentRating: ['safe','suggestive','erotica','pornographic'], // ['safe','etc...'], undefined for default behavior
+    chapterTranslatedLanguage: ['en'], // ['en','es','etc...'], undefined for all languages
 }   
 
 async function pollMangadex (lists) {
@@ -45,15 +53,15 @@ async function pollMangadex (lists) {
 async function fetchChapters (lists, options) {
     try {
         let countFoundManga = 0, countMissingManga = 0, countFoundChapter = 0, countMissingChapter = 0;
-        const mangas = lists[1][0]; // points to mangas tagged as reading
+        const mangas = lists[options.MAL_list][options.MAL_status]; // points to preferred search
         console.log('\n>> Now searching for manga >>\n'); 
         for (const manga of mangas) { // going through mangas
-            const mangaResponse = await axios.get(`https://api.mangadex.org/manga`, { // fetching mangas tagged as reading
+            const mangaResponse = await axios.get(`https://api.mangadex.org/manga`, { // fetching MAL mangas based on preference
                 params: {
-                    title: manga.node.title, // MAL mangas tagged as reading
-                    contentRating: ['safe','suggestive','erotica','pornographic'], // includes all contentRatings
-                    'order[relevance]': 'desc', // order by most relevant to least relevant
-                    limit: undefined
+                    title: manga.node.title, // manga title
+                    limit: options.limit_manga, // preferred fetch length 
+                    [`order[${options.mangaOrderType}]`]: options.mangaOrderDirection, // e.g 'order[relevance]': 'desc' - orders by most relevant to least relevant
+                    contentRating: options.contentRating // includes preferred contentRatings
                 }
             }); 
             console.log(`-> ${manga.node.title}\n`); // logging MAL manga title
@@ -61,16 +69,16 @@ async function fetchChapters (lists, options) {
                 console.log('> Manga was not found');
                 countMissingManga++;
             } else { 
-                const chapterResponse = await axios.get('https://api.mangadex.org/chapter', { // fetching english chapters of manga
+                const chapterResponse = await axios.get('https://api.mangadex.org/chapter', { // fetching chapters of manga
                     params: {
                         manga: mangaResponse.data.data[0].id, // id taken from prior manga endpoint fetch
-                        translatedLanguage: ['en'], // ['en'], // filter english translations
-                        'order[chapter]': 'desc', // order by newest chapter
-                        limit: undefined
+                        limit: options.limit_chapter, // preferred fetch length 
+                        [`order[${options.chapterOrderType}]`]: options.chapterOrderDirection, // e.g 'order[chapter]': 'desc' - orders by newest to oldest chapter
+                        translatedLanguage: options.chapterTranslatedLanguage // filter by preferred translation
                     }
                 });
-                if (!chapterResponse.data.data.length) { // if manga had no english chapters
-                    console.log('> No english chapters found');
+                if (!chapterResponse.data.data.length) { // if manga had no chapters for given parameters
+                    console.log('> No chapters found');
                     countMissingChapter++;
                 } else {
                     for (let i = 0; i < chapterResponse.data.data.length; i++) { // logging all found chapters
