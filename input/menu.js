69398771a893812f3d1f76547e2d7c37fd78992c
markalterego <122254234+markalterego@users.noreply.github.com>
2025-07-25 19:49:08 +0300

    moved menus to new file located at /input/menu.js+kept working on logtoconsole.js

---
+++ input/menu.js
@@ -0,0 +1,251 @@
+import { pollMAL } from "../fetch/pollMAL.js";
+import { pollMangadex } from "../fetch/pollMangadex.js";
+import { log } from "../output/logtoconsole.js";
+import readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
+import { filehandle } from "../filehandling/filehandle.js";
+
+let lists = null; // holds animelist and mangalist, refer to bottom of file for more info on syntax
+let config = null; // holds user specific options
+let refresh = true; // loops through menu while set to true
+const rl = readline.createInterface({ input, output }); // enabling input/output
+
+async function menu(l, c) {
+    try {
+        lists = l; config = c;
+        process.stdout.write('\x1Bc'); // ANSI for full terminal reset (using in place of cls [this actually works])   
+        while (refresh) {
+            if (config.menuOption==='short') {
+                refresh = await shortMenu(); // displays the short version of the menu
+            } else if (config.menuOption==='long') {
+                refresh = await longMenu(); // displays the long version of the menu
+            } else {
+                console.log('\n|| Given menuOption doesn\'t exist');
+            }
+        }
+    } catch (error) {
+        if (error.code==='ABORT_ERR') console.error(); // extra newline for extra cleanliness :)
+        console.error(`\n||\n|| Error: ${error.message}\n||`); // handles e.g. CTRL + C
+    } finally {
+        rl.close(); 
+    }
+}
+
+async function shortMenu() {
+    let m = 0, r = false; // m = menu, r = refresh
+    
+    while (m !== 8) 
+    {
+        console.log('\n||\n|| What would you like to do?\n||');
+        console.log('|| 0 -> Watching anime');
+        console.log('|| 1 -> Completed anime');
+        console.log('|| 2 -> Reading manga');
+        console.log('|| 3 -> Completed manga');
+        console.log('|| 4 -> Full list');
+        console.log('|| 5 -> Poll mangadex');
+        console.log('|| 6 -> Poll mal');
+        console.log('|| 7 -> Clear screen');
+        console.log('|| 8 -> Exit\n||');
+
+        const userInput = await rl.question('\n|| Input: '); // get user input
+        m = parseInt(userInput, 10); // convert userinput to int
+
+        process.stdout.write('\x1Bc'); // ANSI for full terminal reset (using in place of cls [this actually works])   
+
+        switch (m) 
+        {
+            case 0:
+                await log('00', lists);
+                break;
+            case 1:
+                await log('01', lists);
+                break;
+            case 2:
+                await log('10', lists);
+                break;
+            case 3:
+                await log('11', lists);
+                break;
+            case 4:
+                await log('all', lists);
+                break;
+            case 5:
+                await pollMangadex(lists); // searches for newest chapters   
+                break;
+            case 6:
+                lists = await pollMAL(); // searches and returns MAL lists
+                await filehandle('mal', lists);
+                break;
+            case 7:
+                process.stdout.write('\x1Bc'); // ANSI for full terminal reset (using in place of cls [this actually works])   
+                break;
+            case 8:
+                break;
+            case 888:
+                await settingsMenu(); 
+                r = true; m = 8; // goes out of loop and refreshes menu
+                break;
+            default:
+                console.log('\n|| Please input a valid option');
+        }
+    }
+
+    return r;
+}
+
+async function longMenu() {
+    let m = 0, r = false; // m = menu, r = refresh
+    
+    while (m !== 14) 
+    {
+        console.log('\n||\n|| What would you like to do?\n||');
+        console.log('|| 0 -> Watching anime');
+        console.log('|| 1 -> Completed anime');
+        console.log('|| 2 -> On hold anime');
+        console.log('|| 3 -> Dropped anime');
+        console.log('|| 4 -> Plan to watch anime');
+        console.log('|| 5 -> Reading manga');
+        console.log('|| 6 -> Completed manga');
+        console.log('|| 7 -> On hold manga');
+        console.log('|| 8 -> Dropped manga');
+        console.log('|| 9 -> Plan to read manga');
+        console.log('|| 10 -> All of the above');
+        console.log('|| 11 -> Poll mangadex');
+        console.log('|| 12 -> Poll mal');
+        console.log('|| 13 -> Clear screen');
+        console.log('|| 14 -> Exit\n||');
+
+        const userInput = await rl.question('\n|| Input: '); // get user input
+        m = parseInt(userInput, 10); // convert userinput to int
+
+        process.stdout.write('\x1Bc'); // ANSI for full terminal reset (using in place of cls [this actually works])   
+
+        switch (m) 
+        {
+            case 0:
+                await log('00', lists);
+                break;
+            case 1:
+                await log('01', lists);
+                break;
+            case 2:
+                await log('02', lists);
+                break;
+            case 3:
+                await log('03', lists);
+                break;
+            case 4:
+                await log('04', lists);
+                break;
+            case 5:
+                await log('10', lists);
+                break;
+            case 6:
+                await log('11', lists);
+                break;
+            case 7:
+                await log('12', lists);
+                break;
+            case 8:
+                await log('13', lists);
+                break;
+            case 9:
+                await log('14', lists);
+                break;
+            case 10:
+                await log('all', lists);
+                break;
+            case 11:
+                await pollMangadex(lists); // searches for newest chapters   
+                break;
+            case 12:
+                lists = await pollMAL(); // searches and returns MAL lists
+                await filehandle('mal', lists);
+                break;
+            case 13:
+                process.stdout.write('\x1Bc'); // ANSI for full terminal reset (using in place of cls [this actually works])   
+                break;
+            case 14:
+                break;
+            case 888:
+                await settingsMenu(); 
+                r = true; m = 14; // goes out of loop and refreshes menu
+                break;
+            default:
+                console.log('\n|| Please input a valid option');
+        }
+    }
+
+    return r;
+}
+
+async function settingsMenu() {
+    let m = 0;
+    
+    while (m !== 2) 
+    {
+        console.log('\n||\n|| Settings\n||');
+        console.log(`|| 0 -> Toggle ${config.menuOption === 'short' ? 'long' : 'short'}Menu`);
+        console.log(`|| 1 -> Automatically fetch Mangadex when polling (currently ${config.autoFetchMangadex ? 'on' : 'off'})`);
+        console.log('|| 2 -> Return to main menu\n||');
+
+        const userInput = await rl.question('\n|| Input: '); // get user input
+        m = parseInt(userInput, 10); // convert userinput to int
+
+        process.stdout.write('\x1Bc'); // ANSI for full terminal reset (using in place of cls [this actually works])   
+
+        switch (m) 
+        {
+            case 0: 
+                if (config.menuOption === 'short') config = { ...config, menuOption: 'long' }; else config = { ...config, menuOption: 'short' }; // changing menuOption
+                m = 2; await filehandle('config', config); // writes config.file
+                break;
+            case 1:
+                if (config.autoFetchMangadex) config = { ...config, autoFetchMangadex: false }; else config = { ...config, autoFetchMangadex: true }; // toggling autofetching on Mangadex
+                m = 2; await filehandle('config', config); // writes config.file
+                break;
+            case 2:
+                break;
+            default:
+                console.log('\n|| Please input a valid option');
+        }
+    }
+}
+
+export { menu };
+
+/*
+Understanding the layout of lists:
+
+lists[0]... = animelist 
+    ...[0] = watching 
+        ...[0 - ?] = specific series 
+            .node/.list_status = info about series at given index
+    ...[1] = completed
+        ---||---
+    ...[2] = on hold
+        ---||---
+    ...[3] = dropped
+        ---||---
+    ...[4] = plan to watch
+        ---||---
+        
+lists[1]... = mangalist
+    ...[0] = reading
+        ---||---
+    ...[1] = completed
+        ---||---
+    ...[2] = on hold
+        ---||---
+    ...[3] = dropped
+        ---||---
+    ...[4] = plan to read
+        ---||---
+
+e.g. 
+lists[0][0][0].node.title
+lists[0][0][0].list_status.num_episodes_watched
+
+console.log('lists[0][0][0].node.title:', lists[0][0][0].node.title);
+console.log('lists[0][0][0].list_status.num_episodes_watched:', lists[0][0][0].list_status.num_episodes_watched);
+*/
--- output/logtoconsole.js
+++ output/logtoconsole.js
@@ -2,30 +2,53 @@ import { animeStatus, mangaStatus } from "../regular/export.js";
 
 async function log (options, lists) {
     try {
-        let optionsArray = [ // optionsArray fully filled with false
-            Array(animeStatus.length).fill(false),
-            Array(mangaStatus.length).fill(false)
-        ];
-        if (options !== 'all') optionsArray[parseInt(options.charAt(0), 10)][parseInt(options.charAt(1), 10)] = true; // fill optionsArray with true
-        else for (let i = 0; i < optionsArray.length; i++) for (let ii = 0; ii < optionsArray[i].length; ii++) optionsArray[i][ii] = true; // fill a specific point in optionsArray with true
-        await logMAL(optionsArray, lists); // log MAL titles based on optionsArray
+        if (await validateParameters(options, lists)) { // check whether to allow logging
+            let optionsArray = [ // optionsArray fully filled with false
+                Array(animeStatus.length).fill(false),
+                Array(mangaStatus.length).fill(false)
+            ];
+            if (options !== 'all') optionsArray[parseInt(options.charAt(0), 10)][parseInt(options.charAt(1), 10)] = true; // fill a specific point in optionsArray with true
+            else for (let i = 0; i < optionsArray.length; i++) for (let ii = 0; ii < optionsArray[i].length; ii++) optionsArray[i][ii] = true; // fill optionsArray with true
+            await logMAL(optionsArray, lists); // log MAL titles based on optionsArray
+        }
     } catch (error) {
         console.error(`\n||\n|| Error: ${error.message}\n||`);
     }
 }
 
+async function validateParameters (options, lists) {
+    let option1Flag = false, option2Flag = false, listsFlag = false, result = false;
+    
+    const o1 = parseInt(options.charAt(0)); // parsing first option from the string
+    const o2 = parseInt(options.charAt(1)); // parsing second option from the string
+
+    if (o1 === 0 || o1 === 1) option1Flag = true; // flag is set if either 0 or 1
+    if (o2 >= 0 && o2 <= 4) option2Flag = true; // flag is set if between 0 and 4
+    if (lists[o1]?.[o2]?.length > 0) listsFlag = true; // flag is set if entries can be found at given options
+
+    if (!option1Flag) console.log(`\n||\n|| The first parameter has to be either 0 or 1\n||`);
+    if (!option2Flag) console.log(`\n||\n|| The second parameter has to be between 0 and 4\n||`);
+    if (!listsFlag) console.log(`\n||\n|| No matches were found on MAL for the given parameters\n||`)
+    
+    if (option1Flag && option2Flag && listsFlag) result = true;
+
+    return result;
+}
+
 async function logMAL (optionsArray, lists) {    
-    for (let i = 0; i < optionsArray.length; i++) { // go through optionsArray[0 and 1]
+    for (let i = 0, animemangaheaderflag = false; i < optionsArray.length; i++, animemangaheaderflag = false) { // go through optionsArray[0 and 1]
         for (let ii = 0; ii < optionsArray[i].length; ii++) { // go through optionsArray[0 or 1][0 to 4]
+            if (!i && !ii) console.log();
             if (optionsArray[i][ii]) { // if optionsArray[i][ii] is true
                 for (let iii = 0; iii < lists[i][ii].length; iii++) { // log MAL lists from the same point where optionsArray is marked true
-                    if (!iii) console.log(`\n||\n|| (${lists[i][ii][iii].list_status.status.charAt(0).toUpperCase()+lists[i][ii][iii].list_status.status.slice(1)})\n||`); // log list_status at ii
+                    if (!animemangaheaderflag) console.log(`||\n|| == ${!i ? 'Anime' : 'Manga'} ==`); animemangaheaderflag = true; // logs Anime or Manga header at first encounter of one or the other
+                    if (!iii) console.log(`||\n|| (${lists[i][ii][iii].list_status.status.charAt(0).toUpperCase()+lists[i][ii][iii].list_status.status.slice(1)})\n||`); // log list_status at ii
                     const progress = lists[i][ii][iii].node.num_episodes===undefined ? lists[i][ii][iii].list_status.num_chapters_read : lists[i][ii][iii].list_status.num_episodes_watched; // how many episodes watched / chapters read 
                     const length = lists[i][ii][iii].node.num_episodes===undefined ? lists[i][ii][iii].node.num_chapters : lists[i][ii][iii].node.num_episodes; // how many episodes / chapters does the series have
-                    console.log(`|| - ${lists[i][ii][iii].node.title} ( ${progress} / ${length ? length : 'unknown'} )`); // log series
-                    if (iii===lists[i][ii].length-1) console.log('||');
+                    console.log(`|| - ${lists[i][ii][iii].node.title} ${!ii ? (('(' + progress + ' / ') + (length ? length : 'unknown') + ')') : ''}`); // log series
                 }
-            }
+            } 
+            if (i === optionsArray.length - 1 && ii === optionsArray[i].length - 1) console.log('||');
         }
     }
 }
