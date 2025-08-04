9e8f7e5861a04396f1586bc0e2e27417a42a13a2
markalterego <122254234+markalterego@users.noreply.github.com>
2025-08-04 13:14:06 +0300

    modified TODO list

--- main.js
+++ main.js
@@ -31,19 +31,13 @@ main();
 /*
 TODO (or not to do...)
 
-- pollMangadex should poll into a const, the same as pollMAL
-
-- filehandle should get both polling results as input and save that info into 'mal.file' and e.g. 'mangadex.file' respectively
-
 - make it possible to list a series in a category (e.g. watching) and then by inputting the number of the series print out all dates at which the series was updated, essentially see how you progressed along the series
 
 - an option to print out the complete history of every update to MAL (ascending/descending by date)
 
-- make it possible to do custom polling through ui in pollMangadex.js
-
-- make it possible to exclude specific titles when polling Mangadex
-
 - add an option to settings for enable/disable auto search (poll instantly when using pollMangadex.js [no additional menu at pollMangadex.js]) for mangadex polls
 
-- make it possible to do custom log MAL stuff in logtoconsole.js <- add the possibility to log only specific stuff found per item of lists[i][ii] 
+- logtoconsole.js <- add the possibility to log only specific stuff found per item of lists[i][ii] 
+
+- return boolDisplay from customLogMenuMAL as well and save it to config.file, saving boolDisplay's as boolDisplayMAL+boolDisplayMangadex would also make them easier to differentiate 
 */
