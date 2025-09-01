99a60e52bb60699434f1c118ae99a901d0bbfcaf
markalterego <122254234+markalterego@users.noreply.github.com>
2025-09-02 00:20:35 +0300

    started working on fetching manga/chapters from Comick API

---
+++ fetch/fetchComick.js
@@ -0,0 +1,141 @@
+import { page } from '../main.js';
+import { setTimeout } from 'timers/promises';
+
+async function fetchComick (search) {
+    try {
+        await baselinePageSetup(); // setup page
+        // const mangaData = await fetchManga(search); // fetch manga endpoint
+        // const chapterData = await fetchChapter(mangaData); // fetch chapter endpoint
+        // await logData(mangaData, chapterData);
+    } catch (error) {
+        console.error(`\n||\n|| Error: ${error.message}\n||`);
+    }
+}
+
+async function baselinePageSetup() {
+    try {
+        // getting cookies etc. from main page to avoid getting cloudflare blocked later 
+        await page.goto('https://comick.io/'); // go to comick.io
+        await page.waitForSelector('#__next', { timeout: 5000 }); // wait for cloudflare checks
+    } catch (error) {
+        console.error(`\n||\n|| Error: ${error.message}\n||`);
+    }   
+}
+
+async function fetchManga (search) {
+    try {
+        const url = `https://api.comick.io/v1.0/search?${search}`; // mapping search as parameter to url
+        const startTime = performance.now(); // starting timing
+        const data = await page.evaluate(async (url) => { // calling the api
+            const res = await fetch(url, {
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+            return res.json();
+        }, url); // <-- search inputted here
+        const timeTaken = Math.round(performance.now()-startTime); // avoiding rate-limit
+        if (timeTaken < 250) await setTimeout(250-timeTaken);
+        return data; 
+    } catch (error) {
+        if (error.response) {
+            console.error(`\n||\n|| Error: ${error.response.status}: ${error.response.statusText}: ${error.response.data.message}\n||`);
+        } else {
+            console.error(`\n||\n|| Error: ${error.message}\n||`);
+        }
+    }
+}
+
+async function fetchChapter() {
+
+}
+
+async function logData (mangaData, chapterData) {
+    try {
+        console.log(mangaData);
+        console.log(chapterData);
+        // console.log(`\n-> ${manga_title}\n`);
+        // chapterData.forEach((chapter) => {
+        //     const chapter_num = chapter.chap ? parseInt(chapter.chap, 10) : 'Unknown'; // chapter number 
+        //     const chapter_title = chapter.title ? chapter.title : 'No Title'; // chapter title
+        //     const chapter_hid = chapter.hid ? chapter.hid : false; // needed for url
+        //     const url = chapter_hid ? `https://comick.io/comic/${manga_slug}/${chapter_hid}` : `No URL`; // e.g. https://comick.io/comic/00-sousou-no-frieren/8Qv95pQa-chapter-140-en
+        //     console.log(`> ${chapter_title} - ${chapter_num} - ${url}`);
+        // });
+    } catch (error) {
+        console.error(`\n||\n|| Error: ${error.message}\n||`);        
+    }
+}
+
+export { fetchComick };
+
+
+/*
+    HOX!!!!!!
+    RESPONSES FROM COMICK API MOST LIKELY NOT ACTUALLY ORDERED IN ANY WAY
+
+    Comick API results (manga):
+    
+    -array of objects
+    [
+        {
+            id: num,
+            hid: string,
+            slug: string, (e.g. "00-sousou-no-frieren" - needed for url)
+            title: string,
+            country: string,
+            rating: string,
+            bayesian_rating: string,
+            rating_count: num,
+            follow_count: num,
+            desc: string,
+            status: num,
+            last_chapter: num,
+            translation_completed: boolean,
+            view_count: num,
+            content_rating: string,
+            demographic: num,
+            uploaded_at: string,
+            genres: [num, num, etc...],
+            created_at: string,
+            user_follow_count: num,
+            year: num,
+            mu_comics: { year: num },
+            is_english_title: boolean?,
+            md_titles: [ {title: string}, {title: string}, etc... ],
+            md_covers: [ { w: num, h: num, b2key: string }, etc? ],
+            highlight: string
+        }, 
+        {...},
+    ]
+
+    Comick API (chapter):
+    
+    -object of properties
+    {
+        chapters: 
+        [
+            { 
+                id: num,
+                chap: string,
+                title: string,
+                vol: string,
+                lang: string,
+                created_at: string,
+                updated_at: string,
+                up_count: num,
+                down_count: num,
+                is_the_last_chapter: boolean,
+                publish_at: string,
+                group_name: [ Array??? ], 
+                hid: string, <-- distinct from manga hid
+                identities: [ Object??? ],
+                md_chapters_groups: [ Array??? ]
+            },
+            {...},
+        ]
+        total: num,
+        checkVol2Chap1: boolean, <-- ???
+        limit: num
+    }
+*/
