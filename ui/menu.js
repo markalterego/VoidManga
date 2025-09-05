431b20731a24c14e128f7eb6aa63023440431150
markalterego <122254234+markalterego@users.noreply.github.com>
2025-09-05 21:52:40 +0300

    changed filterEntriesFromMangadexFetch to filterEntriesFromFetch and made it so that the function now allows filtering values by the received key parameter+improved logic at customFetchMenuComick, hence logs are more specific to user's actions

--- ui/menu.js
+++ ui/menu.js
@@ -2,7 +2,9 @@ import { fetchMAL } from "../fetch/fetchMAL.js";
 import { fetchMangadex } from "../fetch/fetchMangadex.js";
 import { customLogMAL } from "./customLogMAL.js";
 import { filehandle } from "../filehandling/filehandle.js";
-import { animeStatus, chapterOrderTypes, chapterTranslatedLanguages, contentRatings, mangaOrderTypes, mangaStatus, orderDirections, fetchMangadexOptions } from "../helpers/export.js";
+import { animeStatus, chapterOrderTypes, chapterTranslatedLanguages, 
+         contentRatings, mangaOrderTypes, mangaStatus, orderDirections, 
+         fetchMangadexOptions, expectedFilters } from "../helpers/export.js";
 import { testFetching } from "../fetch/testFetching.js";
 import { takeUserInput, clearScreen, customFetchMangadexDisplay } from "../helpers/functions.js";
 import { rl } from '../main.js';
@@ -67,7 +69,7 @@ async function rootMenu() {
                 if (!config.autoFetchMangadex) {
                     const returnArr = await customFetchMenuMangadex(); // fetch Mangadex by preference
                     config = { ...config, fetchMangadexOptions: returnArr[0], boolDisplayMangadex: returnArr[1] };
-                    await filehandle('config', config); await filehandle('mal', lists);
+                    await filehandle('config', config); await filehandle('mal', lists); // save config and lists to file
                 } else { 
                     await fetchMangadex(lists, config.fetchMangadexOptions);
                 }
@@ -75,7 +77,7 @@ async function rootMenu() {
             case 5:
                 const toggleStringSearch = await customFetchMenuComick(); // search and log Comick API
                 config = { ...config, toggleStringSearchComick: toggleStringSearch }; // save toggleStringSearch to config
-                await filehandle('config', config); // save config to config.file
+                await filehandle('config', config); await filehandle('mal', lists); // save config and lists to file
                 break;
             case 6:
                 lists = await fetchMAL(); // searches and returns MAL lists
@@ -238,7 +240,7 @@ async function customFetchMenuMangadex() {
         console.log('\n||\n|| Custom fetch Mangadex\n||');
         console.log('|| 0 -> Fetch with options');
         console.log('|| 1 -> Change options');
-        console.log('|| 2 -> Fetch exclusions');
+        console.log('|| 2 -> Filter MAL titles');
         console.log('|| 3 -> Empty options');
         console.log('|| 4 -> Toggle display');
         console.log('|| e -> Return to menu\n||');
@@ -259,7 +261,7 @@ async function customFetchMenuMangadex() {
                 break;
             case 2:
                 // filtering items not wanted to be fetched
-                await filterEntriesFromMangadexFetch();
+                await filterEntriesFromFetch('includeInMangadexFetch');
                 break;
             case 3:
                 // emptying / nullifying all options
@@ -631,107 +633,6 @@ async function changeMangadexOptionMenu (boolDisplay, fetchOptions) {
     }
 }
 
-async function filterEntriesFromMangadexFetch() {
-    let m = 0;
-
-    while (m !== 'e') 
-    {
-        // select where to list statuses from
-        console.log('\n||\n|| What do you want to do?\n||');
-        console.log('|| 0 -> Filter anime');
-        console.log('|| 1 -> Filter manga');
-        console.log('|| 2 -> Include all');
-        console.log('|| 3 -> Exclude all');
-        console.log('|| e -> Go back\n||');
-
-        m = await takeUserInput(); // get user input
-
-        await clearScreen(); // clears console window   
-    
-        // logging statuses by type
-        if (m === 0 || m === 1) {
-            // saving selected type 
-            const type = m;
-            while (m !== 'e') 
-            {
-                console.log('\n||\n|| Select a status\n||');
-                if (type === 0) { // anime
-                    animeStatus.forEach((value, index) => {
-                        console.log(`|| ${index} -> ${value}`);
-                    });
-                } else { // manga
-                    mangaStatus.forEach((value, index) => {
-                        console.log(`|| ${index} -> ${value}`);
-                    });
-                }
-                console.log('|| e -> Go back\n||');
-
-                m = await takeUserInput(); // get user input
-
-                await clearScreen(); // clears console window  
-
-                // logging titles by status
-                if ((type === 0 && m < animeStatus.length) || (type === 1 && m < mangaStatus.length)) { 
-                    // saving the selected status
-                    const status = m;
-                    // going back to upper menu in case lists[type][status] is empty
-                    if (!lists[type][status].length) { 
-                        console.log('\n||\n|| No titles found for the selected status\n||'); 
-                    } else {
-                        while (m !== 'e') 
-                        {
-                            console.log('\n||\n|| Select titles to be fetched\n||')
-                            lists[type][status].forEach((item, index) => {
-                                console.log(`|| ${index} -> ${item.node.title} ${item.includeInMangadexFetch ? '[x]' : '[]'}`); 
-                            });
-                            console.log('|| e -> Go back\n||');             
-
-                            m = await takeUserInput(); // get user input
-
-                            await clearScreen(); // clears console window  
-                            
-                            // toggling filter at given option
-                            if (m > -1 && m < lists[type][status].length) {
-                                const item = lists[type][status][m]; // referring to item
-                                if (item.includeInMangadexFetch) item.includeInMangadexFetch = false; 
-                                else item.includeInMangadexFetch = true;
-                            } else if (m !== 'e') {
-                                console.log('\n|| Please input a valid option');
-                            }
-                        }
-                    }
-                    m = null; // ensuring upper menu doesn't exit
-                } else if (m !== 'e') {
-                    console.log('\n|| Please input a valid option');
-                }
-            }
-            m = null; // ensuring upper menu doesn't exit
-        } else if (m === 2) {
-            // reassigning fetch filters for Mangadex as true
-            for (const type of lists) {
-                for (const status of type) {
-                    for (const item of status) {
-                        item.includeInMangadexFetch = true;
-                    }
-                }
-            }
-            console.log('\n||\n|| Included all titles to fetch\n||');
-        } else if (m === 3) {
-            // reassigning fetch filters for Mangadex as false
-            for (const type of lists) {
-                for (const status of type) {
-                    for (const item of status) {
-                        item.includeInMangadexFetch = false;
-                    }
-                }
-            }
-            console.log('\n||\n|| Excluded all titles from fetch\n||');
-        } else if (m !== 'e') {
-            console.log('\n|| Please input a valid option');
-        }
-    }
-}
-
 async function customFetchMenuComick() {
     let m = 0, count = 0, searchString = '', selectionFound = false;
     let toggleStringSearch = config.toggleStringSearchComick ? config.toggleStringSearchComick : false;
@@ -768,6 +669,7 @@ async function customFetchMenuComick() {
                 console.log('|| - No Titles Selected\n||');
             } else {
                 console.log('||');
+                selectionFound = false;
             }
         } else {
             // log current searchString or '???' if unavailable
@@ -776,7 +678,7 @@ async function customFetchMenuComick() {
 
         console.log(`\n||\n|| Custom fetch Comick (Search Type: ${!toggleStringSearch ? 'MAL' : 'String'})\n||`);
         console.log('|| 0 -> Fetch with options');
-        console.log(`|| 1 -> ${!toggleStringSearch ? 'Exclude from search' : 'Change search'}`);
+        console.log(`|| 1 -> ${!toggleStringSearch ? 'Filter MAL titles' : 'Change search'}`);
         console.log('|| 2 -> Toggle search type');
         console.log('|| e -> Exit\n||');
 
@@ -794,23 +696,22 @@ async function customFetchMenuComick() {
                     } else { // fetch Comick by string
                         mangaData = await fetchComickMangas(searchString); // returns arr
                     } 
-                    if (!mangaData) { 
+                    if (!mangaData?.length > 0) { 
                         console.log('\n||\n|| Manga endpoint returned no results\n||');
                     } else {
                         selectedMangas = await selectMangasFromFetchResults(mangaData); // select mangas to use in fetch
-                        if (selectedMangas?.length === 0) console.log('\n||\n|| No mangas were selected\n||');
-                        else chapterData = await fetchComickChapters(selectedMangas); // fetching chapters
-                    }
-                    if (!chapterData) {
-                        console.log('\n||\n|| Chapter endpoint returned no results\n||');
-                    } else {
-                        await logComick(selectedMangas, chapterData); // log fetched data
+                        if (!selectedMangas?.length > 0) console.log('\n||\n|| No mangas were selected\n||');
+                        else {
+                            chapterData = await fetchComickChapters(selectedMangas); // fetching chapters
+                            if (!chapterData?.chapters?.length > 0) console.log('\n||\n|| Chapter endpoint returned no results\n||');
+                            else await logComick(selectedMangas, chapterData); // log fetched data
+                        } 
                     }
                     break;
                 }
             case 1:
                 if (!toggleStringSearch) { 
-                    // <-- Function to include/exlude MAL titles from fetch
+                    await filterEntriesFromFetch('includeInComickFetch'); // include/exlude MAL titles from fetch
                 } else { 
                     // <-- Function to change searchString
                 }
@@ -884,6 +785,112 @@ async function selectMangasFromFetchResults (mangaData) {
     }
     // return array containing selected mangas
     return selectedMangas;
+}
+
+async function filterEntriesFromFetch (key) { 
+    const isValidFilterKey = expectedFilters.some(expectedKey => key === expectedKey);
+    let m = 0;
+
+    if (!isValidFilterKey) { // function parameter is not an expected value
+        console.log(`\n||\n|| The received value '${key}' is not valid\n||`);
+    } else { // function parameter is an expected value
+        while (m !== 'e') 
+        {
+            // select where to list statuses from
+            console.log(`\n||\n|| Filtering ${key}:\n||`);
+            console.log('|| 0 -> Filter anime');
+            console.log('|| 1 -> Filter manga');
+            console.log('|| 2 -> Include all');
+            console.log('|| 3 -> Exclude all');
+            console.log('|| e -> Go back\n||');
+
+            m = await takeUserInput(); // get user input
+
+            await clearScreen(); // clears console window   
+        
+            // logging statuses by type
+            if (m === 0 || m === 1) {
+                // saving selected type 
+                const type = m;
+                while (m !== 'e') 
+                {
+                    console.log('\n||\n|| Select a status\n||');
+                    if (type === 0) { // anime
+                        animeStatus.forEach((value, index) => {
+                            console.log(`|| ${index} -> ${value}`);
+                        });
+                    } else { // manga
+                        mangaStatus.forEach((value, index) => {
+                            console.log(`|| ${index} -> ${value}`);
+                        });
+                    }
+                    console.log('|| e -> Go back\n||');
+
+                    m = await takeUserInput(); // get user input
+
+                    await clearScreen(); // clears console window  
+
+                    // logging titles by status
+                    if ((type === 0 && m < animeStatus.length) || (type === 1 && m < mangaStatus.length)) { 
+                        // saving the selected status
+                        const status = m;
+                        // going back to upper menu in case lists[type][status] is empty
+                        if (!lists[type][status].length) { 
+                            console.log('\n||\n|| No titles found for the selected status\n||'); 
+                        } else {
+                            while (m !== 'e') 
+                            {
+                                console.log('\n||\n|| Select titles to be fetched\n||')
+                                lists[type][status].forEach((item, index) => {
+                                    console.log(`|| ${index} -> ${item.node.title} ${item[key] ? '[x]' : '[]'}`); 
+                                });
+                                console.log('|| e -> Go back\n||');             
+
+                                m = await takeUserInput(); // get user input
+
+                                await clearScreen(); // clears console window  
+                                
+                                // toggling filter at given option
+                                if (m > -1 && m < lists[type][status].length) {
+                                    const item = lists[type][status][m]; // referring to item
+                                    if (item[key]) item[key] = false; 
+                                    else item[key] = true;
+                                } else if (m !== 'e') {
+                                    console.log('\n|| Please input a valid option');
+                                }
+                            }
+                        }
+                        m = null; // ensuring upper menu doesn't exit
+                    } else if (m !== 'e') {
+                        console.log('\n|| Please input a valid option');
+                    }
+                }
+                m = null; // ensuring upper menu doesn't exit
+            } else if (m === 2) {
+                // reassigning fetch filters as true
+                for (const type of lists) {
+                    for (const status of type) {
+                        for (const item of status) {
+                            item[key] = true;
+                        }
+                    }
+                }
+                console.log('\n||\n|| Included all titles to fetch\n||');
+            } else if (m === 3) {
+                // reassigning fetch filters as false
+                for (const type of lists) {
+                    for (const status of type) {
+                        for (const item of status) {
+                            item[key] = false;
+                        }
+                    }
+                }
+                console.log('\n||\n|| Excluded all titles from fetch\n||');
+            } else if (m !== 'e') {
+                console.log('\n|| Please input a valid option');
+            }
+        }
+    }
 }
 
 export { menu };
