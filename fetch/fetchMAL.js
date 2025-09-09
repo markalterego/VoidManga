f3a88a434e5194c65135626a8de332378d097750
markalterego <122254234+markalterego@users.noreply.github.com>
2025-09-09 03:46:39 +0300

    re-wrote code used for combining anime and manga fetches to a single entity lists + upon fetching MAL, a function runs on each iteration which checks if either includeInMangadexFetch or includeInComickFetch tags were previously set and if so, will re-apply those old values to the new fetched information

--- fetch/fetchMAL.js
+++ fetch/fetchMAL.js
@@ -5,26 +5,13 @@ import { animeStatus, mangaStatus } from "../helpers/export.js";
 
 dotenv.config();
 
-let animemangalist = [
-    Array(animeStatus.length).fill(null).map(() => []), // animelist
-    Array(animeStatus.length).fill(null).map(() => [])  // mangalist
-]; 
-
-async function fetchMAL() {
-    const startTime = performance.now();
-
-    console.log(`\n||\n|| Now fetching MAL lists for \'${process.env.MAL_USERNAME}\'\n||`);
-    
-    await fetchSeries(); // fetching user's MAL lists
-    const fetchMALTimeTaken = Math.round(performance.now()-startTime); // how long did fetching the anime-/mangalist take
-
-    console.log(`\n||\n|| Fetching user's MAL lists took ${Number(fetchMALTimeTaken/1000).toFixed(3)}s\n||`);
-
-    return animemangalist;
-}
-
-async function fetchSeries() {
+async function fetchMAL (old_lists) {
     try {
+        console.log(`\n||\n|| Now fetching MAL lists for \'${process.env.MAL_USERNAME}\'\n||`);
+        
+        const startTime = performance.now(); // saving when started fetching
+
+        // fetch Anime endpoint
         const malResponseAnime = await axios.get(`https://api.myanimelist.net/v2/users/${process.env.MAL_USERNAME}/animelist`, {
             params: {
                 fields: 'list_status,num_episodes',
@@ -35,8 +22,10 @@ async function fetchSeries() {
                 'X-MAL-CLIENT-ID': process.env.MAL_API_CLIENT_ID
             }
         });
+        const animeData = malResponseAnime.data.data;
         await setTimeout(20); // avoiding rate limit
 
+        // fetch Manga endpoint
         const malResponseManga = await axios.get(`https://api.myanimelist.net/v2/users/${process.env.MAL_USERNAME}/mangalist`, {
             params: {
                 fields: 'list_status,num_chapters',
@@ -47,9 +36,18 @@ async function fetchSeries() {
                 'X-MAL-CLIENT-ID': process.env.MAL_API_CLIENT_ID
             }
         });
+        const mangaData = malResponseManga.data.data;
         await setTimeout(20); // avoiding rate limit
 
-        await sortSeriesByStatus(malResponseAnime.data.data, malResponseManga.data.data);
+        // how long did fetching the anime-/mangalist take
+        const fetchMALTimeTaken = Math.round(performance.now()-startTime); 
+        console.log(`\n||\n|| Fetching user's MAL lists took ${Number(fetchMALTimeTaken/1000).toFixed(3)}s\n||`);
+
+        // format anime- and manga lists
+        const formattedLists = await sortSeriesByStatus(animeData, mangaData, old_lists);
+        
+        // return formatted lists
+        return formattedLists;
     } catch (error) {
         if (error.response) {
             console.error(`\n||\n|| Error: ${error.response.status}: ${error.response.statusText}: ${error.response.data.message}\n||`);
@@ -59,27 +57,61 @@ async function fetchSeries() {
     }
 }
 
-async function sortSeriesByStatus (animelist, mangalist) {
-    for (let i = 0; i < animeStatus.length; i++) { // sort animelist by status
-        for (let ii = 0, iii = 0; ii < animelist.length; ii++) { // sort by all series corresponding to animeStatus[i]
-            if (animelist[ii].list_status.status === animeStatus[i]) { // if status of anime at point ii same as animestatus
-                animemangalist[0][i][iii] = animelist[ii]; 
-                animemangalist[0][i][iii].includeInMangadexFetch = true;
-                animemangalist[0][i][iii].includeInComickFetch = false;
-                iii++; 
+async function sortSeriesByStatus (animelist, mangalist, old_lists) {
+    const animemangalist = [
+        Array(animeStatus.length).fill(null).map(() => []), // animelist
+        Array(animeStatus.length).fill(null).map(() => [])  // mangalist
+    ]; 
+    const ANIME = 0, MANGA = 1;
+    animeStatus.forEach((status, status_index) => { // anime statuses
+        animelist.forEach(async (entry) => { // entries
+            const entry_status = entry.list_status.status;
+            if (entry_status === status) { // same status found
+                const entry_title = entry.node.title; // MAL_title
+                const result = await handleFilters(ANIME, entry_title, old_lists); // get filters
+                const entry_final = { ...entry, 
+                                        includeInMangadexFetch: result.includeInMangadexFetch, 
+                                        includeInComickFetch: result.includeInComickFetch };
+                animemangalist[ANIME][status_index].push(entry_final);
+            }
+        })
+    });
+    mangaStatus.forEach((status, status_index) => { // manga statuses
+        mangalist.forEach(async (entry) => { // entries
+            const entry_status = entry.list_status.status;
+            if (entry_status === status) { // same status found
+                const entry_title = entry.node.title; // MAL_title
+                const result = await handleFilters(MANGA, entry_title, old_lists); // get filters
+                const entry_final = { ...entry, 
+                                        includeInMangadexFetch: result.includeInMangadexFetch, 
+                                        includeInComickFetch: result.includeInComickFetch };
+                animemangalist[MANGA][status_index].push(entry_final);
+            }
+        })
+    });
+    return animemangalist;
+}
+
+async function handleFilters (animeOrManga, title, old_lists) {
+    // the point of handleFilters is to retain the values of 
+    // includeInMangadexFetch & includeInComickFetch, to avoid
+    // having to re-apply the filters on each fetch
+    // note: this should work even when you move a title from e.g. reading/dropped 
+    //       in a way that it still retains the filter for that title
+    // note2: from my understanding, there shouldn't be identical titles at MAL under the
+    //        the same type of list (anime/manga) but if there are, this will not work as expected
+    let result = { includeInMangadexFetch: true, includeInComickFetch: false };
+    if (Array.isArray(old_lists)) {
+        for (const status of old_lists[animeOrManga]) { // go through old list
+            for (const entry of status) { // entry of old list
+                if (entry.node.title === title) { // if same title is found in old list
+                    result.includeInMangadexFetch = entry.includeInMangadexFetch; 
+                    result.includeInComickFetch = entry.includeInComickFetch;
+                }
             }
         }
     }
-    for (let i = 0; i < mangaStatus.length; i++) { // sort mangalist by status
-        for (let ii = 0, iii = 0; ii < mangalist.length; ii++) { // sort by all series corresponding to mangaStatus[i]
-            if (mangalist[ii].list_status.status === mangaStatus[i]) { // if status of manga at point ii same as mangastatus
-                animemangalist[1][i][iii] = mangalist[ii]; 
-                animemangalist[1][i][iii].includeInMangadexFetch = true;
-                animemangalist[1][i][iii].includeInComickFetch = false;
-                iii++; 
-            }
-        }
-    }
+    return result;
 }
 
 export { fetchMAL };
