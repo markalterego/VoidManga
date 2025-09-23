908fd17f1b4d0193a5e60876dc712380b84fb372
markalterego <122254234+markalterego@users.noreply.github.com>
2025-09-23 04:35:50 +0300

    moved menu for opening found chapters for fetchResults from fetchMangadex to menuFetchMangadex and renamed it to openChaptersInBrowserMenu + added entry to TODO list

--- ui/menuFetchMangadex.js
+++ ui/menuFetchMangadex.js
@@ -1,12 +1,19 @@
+import open from 'open';
 import { takeUserInput, customFetchMangadexDisplay, menuFetchFiltersDisplay } from "../helpers/functions.js";
 import { chapterOrderTypes, chapterTranslatedLanguages, contentRatings, 
          mangaOrderTypes, orderDirections, fetchMangadexOptions } from "../helpers/export.js";
 import { filterEntriesFromFetch } from './menuFetchFilters.js';
-import { fetchMangadexMangas, fetchMangadexChapters, openMangadexChaptersInBrowser } from '../fetch/fetchMangadex.js';
+import { fetchMangadexMangas, fetchMangadexChapters } from '../fetch/fetchMangadex.js';
 
 async function menuFetchMangadex (lists, config) {
     const options = !config?.fetchMangadexOptions ? JSON.parse(JSON.stringify(fetchMangadexOptions)) : config.fetchMangadexOptions;
     let m = 0;
+
+    // TODO:
+    // - make it possible to fetch chapters from range by manipulating the 
+    //   offset parameter when fetching chapters---make sure the user only 
+    //   has to provide a lower and upper limit and everything else is handled
+    //   automatically 
 
     while (m !== 'e') 
     {
@@ -39,7 +46,7 @@ async function menuFetchMangadex (lists, config) {
                     if (!hasChapters) { // no chapters found
                         console.log('\n||\n|| No chapters were found\n||');
                     } else {
-                        await openMangadexChaptersInBrowser(combinedData); // logging fetched data
+                        await openChaptersInBrowserMenu(combinedData); // logging fetched data
                     }
                 }
                 break;
@@ -398,4 +405,172 @@ async function selectMangasFromFetchResults (mangaSearches) {
     return selectedMangas;
 }
 
+async function openChaptersInBrowserMenu (fetchResults) {
+    let m = null;
+    // console.dir(fetchResults, {depth: null});
+
+    // TODO:
+    // - consider formatting stuff earlier in code e.g. separate formatting function
+    //   for taking first mangatitle's etc.etc.....
+    // - consider moving this function to menuFetchMangadex.js instead
+
+    while (m !== 'e') {   
+        console.log('\n||\n|| Open chapter in browser:');
+        let searchIndex = 0, selectableIndex = 0; // used for formatting
+        for (const search of fetchResults) {
+            const manga = search.manga;
+            const mangaTitle = Object.values(manga.attributes.title)[0]; // first title of titles
+            const query = search.query;
+            console.log(`||\n|| ${mangaTitle}:\n||`);
+            for (const chapter of search.chapters) {
+                const title = chapter.attributes.title ? chapter.attributes.title : 'No Title'; // title
+                const chNum = chapter.attributes.chapter !== null ? chapter.attributes.chapter : -1; // chapter number
+                const transLang = chapter.attributes.translatedLanguage ? chapter.attributes.translatedLanguage : 'No Translated Language'; // translated language
+                const unreadTag = query.type === 'manga' && // is manga
+                                  query.id === parseInt(manga.attributes.links?.mal, 10) && // is same id 
+                                  query.progress < chNum ? // progress < chNum
+                                  '- {( Unread! )}' : ''; 
+                console.log(`|| ${selectableIndex++}: ${chNum >= 0 ? `Chapter: ${chNum} -` : ''} ${title} (${transLang}) ${unreadTag}`);
+            }
+            if (search.chapters.length === 0) {
+                console.log('|| - No chapters found');
+            }
+            if (searchIndex === fetchResults.length - 1) console.log('||');
+            searchIndex++; 
+        }
+        console.log('\n||\n|| e -> Go back\n||');
+        
+        m = await takeUserInput(); // get user input
+
+        // handle user input
+        if (m >= 0 && m <= selectableIndex - 1) {
+            let i = 0; 
+            for (const search of fetchResults) { // searches
+                for (const chapter of search.chapters) { // chapters
+                    if (i === m) { // highest selectable index
+                        await open(chapter.link) // open chapter in browser
+                    }
+                    i++;
+                }
+            }
+        } else if (m !== 'e') {
+            console.log('\n|| Please input a valid option');
+        }    
+        selectableIndex = 0;
+    }
+
+}
+
 export { menuFetchMangadex };
+
+
+/*
+    fetchResults complete layout (2025/09/23):
+    [   
+        {
+            query: {
+                title: string,
+                id: num,
+                type: string, (anime/manga)
+                progress: num
+            },
+            manga: {
+                id: string,
+                type: string,
+                attributes: {
+                    title: {
+                        ?: string, (language code)
+                        etc...                    
+                    },
+                    altTitles: [
+                        ?: string, (language code)
+                        etc...
+                    ],
+                    description: {
+                        ?: string, (language code)
+                        etc...
+                    },
+                    isLocked: boolean,
+                    links: {
+                        ?: string, (e.g. mal)
+                        etc...
+                    },
+                    originalLanguage: string,
+                    lastVolume: string,
+                    lastChapter: string,
+                    publicationDemographic: string, (e.g. shounen)
+                    status: string, (e.g. completed)
+                    year: num,
+                    contentRating: string, (e.g. suggestive)
+                    tags: [
+                        {
+                            id: string
+                            type: string
+                            attributes: {
+                                name: {
+                                    ?: string (e.g. action)
+                                },
+                                description: {
+                                    ?: string
+                                },
+                                group: string,
+                                version: num
+                            },       
+                            relationships: [
+                                ?: ?
+                            ]
+                        },
+                        etc...
+                    ],
+                    state: string, (e.g. published)
+                    chapterNumbersResetOnNewVolume: boolean,
+                    createdAt: string,
+                    updatedAt: string,
+                    version: num,
+                    availableTranslatedLanguages: [
+                        ?: string, (e.g. en)
+                        etc...
+                    ],
+                    latestUploadedChapter: string, (id of latest uploaded chapter)
+                }, // attributes end
+                relationships: [
+                    {
+                        id: string,
+                        type: string (e.g. author)
+                    },
+                    etc...
+                ]
+            }, // manga end
+            chapters: [
+                {
+                    id: string,
+                    type: string,
+                    attributes: {
+                        volume: string,
+                        chapter: string,
+                        title: string,
+                        translatedLanguage: string,
+                        externalUrl: ???,
+                        isUnavailable: boolean,
+                        publishAt: string,
+                        readableAt: string,
+                        createdAt: string,
+                        updatedAt: string,
+                        pages: num,
+                        version: num
+                    },
+                    relationships: [
+                        {
+                            id: string,
+                            type: string
+                        },
+                        etc...
+                    ],
+                    link: string
+                },
+                etc...
+            ] 
+        }, // fetchResult end
+        etc...
+    ]
+*/
