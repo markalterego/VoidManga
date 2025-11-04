c58f47ea850e2a1e9e9de078e351146f490d8bb0
markalterego <122254234+markalterego@users.noreply.github.com>
2025-11-04 23:02:01 +0200

    re-arranged fetchMAL to smaller functions + started planning how to implement OAuth2 authentication + added a few todos...

--- fetch/fetchMAL.js
+++ fetch/fetchMAL.js
@@ -5,14 +5,39 @@ import { animeStatus, mangaStatus } from "../helpers/export.js";
 
 dotenv.config();
 
-async function fetchMAL (old_lists) {
-    try {
-        console.log(`\n||\n|| Now fetching MAL lists for \'${process.env.MAL_USERNAME}\'\n||`);
-        
-        const startTime = performance.now(); // saving when started fetching
+// TODO:
+// - re-arrange this file into smaller/clearer functions
+// - somehow implement user OAuth2 authentication
+// - allow limited functionality without explicit authentication
+//   when authentication is not possible/not wanted by the user
 
-        // fetch Anime endpoint
-        const malResponseAnime = await axios.get(`https://api.myanimelist.net/v2/users/${process.env.MAL_USERNAME}/animelist`, {
+/*
+
+1.1 if MAL_USERNAME is not defined, check authentication status
+1.2. 
+
+fetchMAL <-- base function
+- include timings in here
+
+getAuthenticationToken <-- implement this
+*/
+
+async function fetchMAL (old_lists) {
+    const username = process.env.MAL_USERNAME;
+    if (!username?.length) {
+        console.log(`\n||\n|| MAL_USERNAME was not defined at .env file\n||`);
+    } else {
+        console.log(`\n||\n|| Now fetching MAL lists for ${username}\n||`);
+        const animelist = await fetchAnimeList(username); // fetch Anime endpoint
+        const mangalist = await fetchMangaList(username); // fetch Manga endpoint
+        const formattedLists = sortSeriesByStatus(animelist, mangalist, old_lists); // format anime- and manga lists
+        return formattedLists; // return formatted lists
+    }   
+}
+
+async function fetchAnimeList (username) {
+    try {
+        const malResponseAnime = await axios.get(`https://api.myanimelist.net/v2/users/${username}/animelist`, {
             params: {
                 fields: 'list_status,num_episodes',
                 limit: 1000, // max value
@@ -21,33 +46,8 @@ async function fetchMAL (old_lists) {
             headers: {
                 'X-MAL-CLIENT-ID': process.env.MAL_API_CLIENT_ID
             }
-        });
-        const animeData = malResponseAnime.data.data;
-        await setTimeout(20); // avoiding rate limit
-
-        // fetch Manga endpoint
-        const malResponseManga = await axios.get(`https://api.myanimelist.net/v2/users/${process.env.MAL_USERNAME}/mangalist`, {
-            params: {
-                fields: 'list_status,num_chapters',
-                limit: 1000, // max value
-                nsfw: true // allows a more accurate response
-            },
-            headers: {
-                'X-MAL-CLIENT-ID': process.env.MAL_API_CLIENT_ID
-            }
-        });
-        const mangaData = malResponseManga.data.data;
-        await setTimeout(20); // avoiding rate limit
-
-        // how long did fetching the anime-/mangalist take
-        const fetchMALTimeTaken = Math.round(performance.now()-startTime); 
-        console.log(`\n||\n|| Fetching user's MAL lists took ${Number(fetchMALTimeTaken/1000).toFixed(3)}s\n||`);
-
-        // format anime- and manga lists
-        const formattedLists = await sortSeriesByStatus(animeData, mangaData, old_lists);
-        
-        // return formatted lists
-        return formattedLists;
+        }).then(await setTimeout(100)); // avoid rate-limit
+        return malResponseAnime.data.data;
     } catch (error) {
         if (error.response) {
             console.error(`\n||\n|| Error: ${error.response.status}: ${error.response.statusText}: ${error.response.data.message}\n||`);
@@ -57,7 +57,30 @@ async function fetchMAL (old_lists) {
     }
 }
 
-async function sortSeriesByStatus (animelist, mangalist, old_lists) {
+async function fetchMangaList (username) {
+    try {
+        const malResponseManga = await axios.get(`https://api.myanimelist.net/v2/users/${username}/mangalist`, {
+            params: {
+                fields: 'list_status,num_chapters',
+                limit: 1000, // max value
+                nsfw: true // allows a more accurate response
+            },
+            headers: {
+                'X-MAL-CLIENT-ID': process.env.MAL_API_CLIENT_ID
+            }
+        }).then(await setTimeout(100)); // avoid rate-limit
+        return malResponseManga.data.data; 
+    } catch (error) {
+        console.log(error.response);
+        if (error.response) {
+            console.error(`\n||\n|| Error: ${error.response.status}: ${error.response.statusText}: ${error.response.data.message}\n||`);
+        } else {
+            console.error(`\n||\n|| Error: ${error.message}\n||`);
+        }
+    }
+}
+
+function sortSeriesByStatus (animelist, mangalist, old_lists) {
     const animemangalist = [
         Array(animeStatus.length).fill(null).map(() => []), // animelist
         Array(animeStatus.length).fill(null).map(() => [])  // mangalist
@@ -80,7 +103,7 @@ async function sortSeriesByStatus (animelist, mangalist, old_lists) {
             const entry_status = entry.list_status.status;
             if (entry_status === status) { // same status found
                 const entry_title = entry.node.title; // MAL_title
-                const result = await handleFilters(MANGA, entry_title, old_lists); // get filters
+                const result = handleFilters(MANGA, entry_title, old_lists); // get filters
                 const entry_final = { ...entry, 
                                         includeInMangadexFetch: result.includeInMangadexFetch };
                 animemangalist[MANGA][status_index].push(entry_final);
@@ -90,7 +113,7 @@ async function sortSeriesByStatus (animelist, mangalist, old_lists) {
     return animemangalist;
 }
 
-async function handleFilters (animeOrManga, title, old_lists) {
+function handleFilters (animeOrManga, title, old_lists) {
     // the point of handleFilters is to retain the value of 
     // includeInMangadexFetch to avoid having to re-apply the
     // filters on each fetch
