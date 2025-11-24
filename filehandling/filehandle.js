f3d4050fbf71cd7878e799c7f6e573e002a03692
markalterego <122254234+markalterego@users.noreply.github.com>
2025-11-25 01:49:40 +0200

    created new function writeEnv for the purpose of creating and updating the .env file at the project root folder + both functions at filehandle.js - filehandle() and writeEnv() now use synchronous writing/reading functions (readFileSync/writeFileSync) for clarity since the former asynchronous functions (readFile/writeFile) had no purpose being asynchronous in the first place

--- filehandling/filehandle.js
+++ filehandling/filehandle.js
@@ -1,18 +1,17 @@
-import { existsSync, mkdirSync } from 'fs';
-import { readFile, writeFile } from 'fs/promises';
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 import path from 'path';
 
-async function filehandle (fileIdentifier, input) {
+function filehandle (fileIdentifier, input) {
     try {
         const folderDestination = path.resolve(import.meta.dirname, '../data'); // refers to data folder
         if (!existsSync(folderDestination)) mkdirSync(folderDestination); // creates data folder if it doesn't exist
         const destination = path.resolve(folderDestination, `${fileIdentifier}.file`); // file creation 
         if ((typeof fileIdentifier)==='string') {
             if (!input) { // read file
-                const data = await readFile(destination, 'utf8');
+                const data = readFileSync(destination, 'utf8');
                 return JSON.parse(data);
             } else { // write file
-                await writeFile(destination, JSON.stringify(input, null, 2), 'utf8');
+                writeFileSync(destination, JSON.stringify(input, null, 2), 'utf8');
             }
         } else {
             console.log(`\n||\n|| The given fileIdentifier has to be of type string\n||`);
@@ -22,4 +21,23 @@ async function filehandle (fileIdentifier, input) {
     }
 }
 
-export { filehandle };
+function writeEnv (input) {
+    try {
+        const folderDestination = path.resolve(import.meta.dirname, '..'); // refers to pollManga folder
+        const destination = path.resolve(folderDestination, '.env'); // refers to .env file
+        const env = readFileSync(destination, 'utf-8').split('\n'); // env = [ "key1=value1", "key2=value2" ]
+        for (const key in input) {
+            const index = env.findIndex(line => line.startsWith(`${key}=`)); // find existing index of key
+            const value = `${key}=${input[key]}`; // key=value pair as string
+            if (index >= 0) env[index] = value; // found key at env
+            else env.push(value); // append new key=value pair to env
+        }
+        // write .env file by combining each element of env to a single 
+        // string separated by '\n' (the format of an .env file)
+        writeFileSync(destination, env.join('\n'), 'utf8');
+    } catch (error) {
+        console.error(`\n||\n|| Error: ${error.message}\n||`);
+    }
+}
+
+export { filehandle, writeEnv };
