e8b8ab313e27f86172d67598e2317cbdc2c51282
markalterego <122254234+markalterego@users.noreply.github.com>
2025-12-18 04:17:02 +0200

    commented imports related to calling customLogMAL + the first option at rootMenu now calls menuMAL instead

--- ui/menu.js
+++ ui/menu.js
@@ -1,14 +1,15 @@
 import { fetchMAL } from "../fetch/fetchMAL.js";
-import { customLogMAL } from "./customLogMAL.js";
+// import { customLogMAL } from "./customLogMAL.js";
+// import { menuLogMAL } from "./menuLogMAL.js";
+// import { updateMAL } from "../updateMAL/updateMAL.js";
+// import { animeStatus, mangaStatus } from "../helpers/export.js";
+import { menuMAL } from "./menuMAL.js";
 import { filehandle } from "../filehandling/filehandle.js";
 import { testFetching } from "../fetch/testFetching.js";
 import { takeUserInput } from "../helpers/functions.js";
 import { menuFetchMangadex } from "./menuFetchMangadex.js";
-import { menuLogMAL } from "./menuLogMAL.js";
 import { menuLogMangadex } from "./menuLogMangadex.js";
 import { logErrorDetails } from "../helpers/errorLogger.js";
-import { updateMAL } from "../updateMAL/updateMAL.js";
-import { animeStatus, mangaStatus } from "../helpers/export.js";
 
 let lists = null; // holds animelist and mangalist, more info regarding syntax at the bottom of menu.js
 let config = null; // holds user specific options
@@ -33,7 +34,7 @@ async function rootMenu() {
     while (m !== 'e') 
     {
         console.log('\n||\n|| What would you like to do?\n||');
-        console.log('|| 0 -> Log MAL');
+        console.log('|| 0 -> Log/Update MAL');
         console.log('|| 1 -> Log Mangadex');
         console.log(`|| 2 -> Fetch Mangadex`);
         console.log('|| 3 -> Fetch MAL');
@@ -44,11 +45,12 @@ async function rootMenu() {
 
         switch (m) 
         {
-            case 0: {
-                const returnArr = await menuLogMAL(lists, config); // log anime and/or manga by status
-                config = { ...config, logMALOptions: returnArr[0] };
-                filehandle('config', config); 
-                break; }
+            case 0: 
+                lists = await menuMAL(lists);
+                // const returnArr = await menuLogMAL(lists, config); // log anime and/or manga by status
+                // config = { ...config, logMALOptions: returnArr[0] };
+                // filehandle('config', config); 
+                break; 
             case 1: {
                 const options = await menuLogMangadex(mangadexData, lists, config); // <-- log mangadex
                 config = { ...config, logMangadexOptions: options };
