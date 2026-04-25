96888bd6ee0864cf197d0b9c2654f7d9fa206ed0
markalterego <122254234+markalterego@users.noreply.github.com>
2026-04-25 22:31:52 +0300

    created new file fetchUtils.js in /fetch folder + created and exported two new functions withRetry (calls given function until it succeeds or until maxRetries amount of fails is reached in a row) and rateLimitedFetch (calls given function and waits the difference of timeTaken - minMs in case timeTaken was less than the expected amount)

---
+++ fetch/fetchUtils.js
@@ -0,0 +1,30 @@
+import { setTimeout } from "timers/promises";
+
+async function withRetry (fn, maxRetries = 5) {
+    // executes a fetch related function until the 
+    // fetch either succeeds OR fails a set amount
+    // of times in a row
+    for (let attempt = 1; attempt <= maxRetries; attempt++) {
+        try {
+            return await fn();
+        } catch (error) {
+            if (!error.response) throw error;
+            const triesRemaining = maxRetries - attempt;
+            console.error(`|| Fetch failed (Tries remaining: ${triesRemaining ? `${triesRemaining})` : `${triesRemaining})\n||`}`);
+            if (triesRemaining === 0) throw new Error(`${maxRetries} fetches failed in a row`); // throw too many fetches failed in a row err
+        }
+    }
+}
+
+function rateLimitedFetch (fn, minMs = 200) {
+    // executes a function and waits a set amount
+    // of time after the function if the execution
+    // was faster than a set amount of milliseconds
+    const start = performance.now();
+    const result = await fn(); // run function
+    const timeTaken = performance.now() - start;
+    if (timeTaken < minMs) await setTimeout(timeTaken - minMs);
+    return result;
+}
+
+export { withRetry, rateLimitedFetch };
