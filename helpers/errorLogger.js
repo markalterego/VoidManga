07ad89281d647b742280f0fefd0993515d280936
markalterego <122254234+markalterego@users.noreply.github.com>
2026-06-02 21:35:20 +0300

    slightly changed padding above and below logged error

--- helpers/errorLogger.js
+++ helpers/errorLogger.js
@@ -19,7 +19,7 @@ function logErrorDetails (error) {
     const responseHint = data?.hint;
 
     // header
-    console.error(`\n  --- Error ---\n`);
+    console.error(`\n\n  --- Error ---\n`);
 
     // response status + info
     if (status)     console.error(`  Status:     ${status}`);  
@@ -36,7 +36,7 @@ function logErrorDetails (error) {
     }
 
     // footer
-    console.error('\n  -------------\n');
+    console.error('\n  -------------');
 }
 
 export { logErrorDetails };
