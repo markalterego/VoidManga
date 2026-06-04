221f20dc3cce44594ac9712d9cb7ac0910b57a48
markalterego <122254234+markalterego@users.noreply.github.com>
2026-06-04 06:54:15 +0300

    now formatting month by date.getMonth() + 1 since getMonth returns index of month at formatDate

--- helpers/functions.js
+++ helpers/functions.js
@@ -146,7 +146,7 @@ function isMatchingAtStart (matchWith, matchTo) {
 function formatDate (DATE) {
     const date = new Date(DATE);
     const yyyy = date.getFullYear(); 
-    const mm   = String(date.getMonth()).padStart(2, '0');
+    const mm   = String(date.getMonth() + 1).padStart(2, '0'); // getMonth returns index, hence date.getMonth() + 1
     const dd   = String(date.getDay()).padStart(2, '0');
     const hh   = String(date.getHours()).padStart(2, '0');
     const min  = String(date.getMinutes()).padStart(2, '0');
