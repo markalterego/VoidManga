257a663eea7ec7c6e865bdc0ae258242a0b9183b
markalterego <122254234+markalterego@users.noreply.github.com>
2026-06-17 00:36:01 +0300

    init controllerConfig.js, wrote down small TODO inside the file

---
+++ controller/controllerConfig.js
@@ -0,0 +1,17 @@
+
+/*
+TODO:
+  - make logic related saving config to file and
+    updating said config data so that each time 
+    a value changes, updating said value will go 
+    through this file and hence immediately e.g. 
+    save changes to file etc...
+
+P.S. 
+  - this is mostly for making sure that config is 
+    saved to file each time it is updated through 
+    the UI ... there are still many places in the
+    project where logic related to this is simply 
+    different depending on what part of the project
+    you're inspecting
+*/
