702ab4370eba2e7b1bf8bbd21792a34b1babbaf9
markalterego <122254234+markalterego@users.noreply.github.com>
2026-07-07 00:48:56 +0300

    included comments above most exports, changed name of id to getId for clarity

--- helpers/entryHelpers.js
+++ helpers/entryHelpers.js
@@ -6,14 +6,41 @@ TODO:
   - import or simply drag animeStatus/mangaStatus into this file in order
     for getStatus
 */
+
+/**
+ * Index for anime entries at lists.
+ */
 export const ANIME = 0;
+/**
+ * Index for manga entries at lists.
+ */
 export const MANGA = 1;
+/**
+ * 
+ * @param {*} entry Item at lists[type][status].
+ * @returns Index corresponding to ANIME = 0 or MANGA = 1.
+ */
 export const getType = (entry) => entry.node.num_episodes === undefined ? MANGA : ANIME;
+/**
+ * 
+ * @param {*} entry Item at lists[type][status].
+ * @returns String corresponding to ANIME = 'anime' or MANGA = 'manga'.
+ */
 export const getTypeString = (entry) => entry.node.num_episodes === undefined ? 'manga' : 'anime';
+/**
+ * 
+ * @param {*} entry Item at lists[type][status].
+ * @returns Index corresponding to animeStatus/mangaStatus index.
+ */
 export const getStatus = (entry) => getType(entry) === ANIME 
                             ? animeStatus.findIndex(s => s === entry.list_status.status) 
                             : mangaStatus.findIndex(s => s === entry.list_status.status);
+/**
+ * 
+ * @param {*} entry Item at lists[type][status].
+ * @returns String corresponding to animeStatus/mangaStatus string.
+ */
 export const getStatusString = (entry) => getType(entry) === ANIME 
                             ? animeStatus.find(s => s === entry.list_status.status) 
                             : mangaStatus.find(s => s === entry.list_status.status);
-export const id     = (entry) => entry.node.id;
+export const getId = (entry) => entry.node.id;
