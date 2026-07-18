3cc1a7fa2c757d6c6a175c866a9e88e7e11212e6
markalterego <122254234+markalterego@users.noreply.github.com>
2026-07-18 04:51:54 +0300

    created and exported updateConfig function (takes in config, path and value as params ... config is reference to config, path is a string in the format of e.g. 'fetchMangadexOptions.limit_manga' essentially denoting the path to the value inside config being updated ... value is the value being updated to config[path]. The function updates config directly by reference and hence returns nothing, after updating the config, it saves it to config.file through filehandle)

--- controller/controllerConfig.js
+++ controller/controllerConfig.js
@@ -1,17 +1,24 @@
+import { filehandle } from "../filehandling/filehandle.js";
+import { MESSAGE } from "../helpers/export.js";
 
 /*
-TODO:
-  - make logic related saving config to file and
-    updating said config data so that each time 
-    a value changes, updating said value will go 
-    through this file and hence immediately e.g. 
-    save changes to file etc...
-
-P.S. 
-  - this is mostly for making sure that config is 
-    saved to file each time it is updated through 
-    the UI ... there are still many places in the
-    project where logic related to this is simply 
-    different depending on what part of the project
-    you're inspecting
+  The purpose of this controller is mostly for 
+  making sure that config is saved to file each
+  time it is updated through the UI ... 
+  
+  Upon the creation of this file, there are still 
+  many places in the project where logic related
+  to this is simply different depending on what 
+  part of the project you're inspecting.
 */
+
+function updateConfig (config = null, path = '', value = null) {
+    const keys = path.split('.');
+    const lastKey = keys.pop();
+    const parent = keys.reduce((acc, key) => acc?.[key], config);
+    if (parent === undefined) throw new Error(`Failed to update config at '${path}'`);
+    parent[lastKey] = value;
+    filehandle('config', config);
+}
+
+export { updateConfig };
